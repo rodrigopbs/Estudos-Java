d8d195761c57a30f88c27a5f64ced19c7691c61b
rodrigopbs <112721406+rodrigopbs@users.noreply.github.com>
2022-09-17 10:57:53 -0300

    Olá Mundo
    Primeiro programa, teste.

---
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication;
+
+/**
+ *
+ * @author Rodrigo Brandão
+ */
+public class JavaApplication {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Olá Mundo!");
+        // TODO code application logic here
+    }
+    
+}
